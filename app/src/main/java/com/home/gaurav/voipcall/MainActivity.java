3d5c76ac7cfd520231cccfa4d79c2f92a6351753
Gaurav Singh <gaurav26500@gmail.com>
2018-02-23 17:43:30 +0530

    Best Build
    v 1.0

--- app/src/main/java/com/home/gaurav/voipcall/MainActivity.java
+++ app/src/main/java/com/home/gaurav/voipcall/MainActivity.java
@@ -1,7 +1,10 @@
 package com.home.gaurav.voipcall;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button setting = (Button) findViewById(R.id.button6);
+        setting.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,setting_layout.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
